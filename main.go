a9908f6e213833012f44a1a39e6d1c39f6af921c
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-08-18 08:44:30 +0100

    WIP got the long polling working but not cancel, need to figure out why -AP

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	// ctx,cancel := context.WithCancel(context.Background())
+	
+	
+	// ch := make(chan string)
+	
+	
+	http.HandleFunc("/",longpoll)
+	http.ListenAndServe(":8080", nil)
+	
+}
+
+func longpoll(w http.ResponseWriter, r *http.Request){
+	ctx,cancel := context.WithCancel(r.Context())
+	ch := make(chan string)
+	go func( ch chan string, ctxIn context.Context, cancelIn context.CancelFunc){
+		
+		time.Sleep(time.Second * 4)
+		ch <- "message sent!"
+		cancelIn()
+		
+	}(ch, ctx, cancel)
+
+	for{
+		select{
+			case v1 := <-ch:
+				w.Write([]byte(v1))
+				return
+				// cancel()
+			case <-ctx.Done():
+				// wg.Done()
+				log.Fatal("we out!")
+				return
+			}
+	}
 }
